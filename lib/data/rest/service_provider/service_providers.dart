6c5411a5177be5df4ed848a6326226b19b0008ad
artin_mrt <iamartinmrt@gmail.com>
2023-11-23 22:55:36 +0100

    use data from run args

--- lib/data/rest/service_provider/service_providers.dart
+++ lib/data/rest/service_provider/service_providers.dart
@@ -19,7 +19,7 @@ final serviceDioProvider = Provider<Dio>((ref) {
   /// Monitor every usage of this Dio and log every action
   dio.interceptors.add(QueuedInterceptorsWrapper(
     onRequest: (options, handler) {
-      options.headers["Authorization"] = "ApiKey 7968af1a-f683-4fd6-8ce3-6a7a896bbb56";
+      options.headers["Authorization"] = "ApiKey ${EnvHandler.instance.envProperties.apiKey}";
       /// If any API has an extra data containing a custom timeout, then change
       /// the timeout to what was past on that API
       if (options.extra.containsKey(Constants.customTimeOut)) {
